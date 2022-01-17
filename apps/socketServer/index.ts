8d207a4c682edbcbd46081c0b2b547f553cd70b6
dvnkboi <aymandb@outlook.com>
2022-01-17 09:25:49 +0100

    v0.2 unit test implementation

--- apps/socketServer/index.ts
+++ apps/socketServer/index.ts
@@ -1,9 +1,10 @@
+import { map } from 'shared';
 const express = require('express')
 const app = express()
 const port = 3002
 
 app.get('/', (_req: any, res: { send: (arg0: string) => void }) => {
-  res.send('Hello World!')
+  res.send('Hello World!' + map(1, 0, 10, 0, 100));
 })
 
 app.listen(port, () => {
