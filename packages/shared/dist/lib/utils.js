6f9497f3772bb7739695303688319dd7842e7330
AymanRamy25 <ayman.ramy2017@gmail.com>
2022-02-04 00:34:54 +0100

    v0.9.env

--- packages/shared/dist/lib/utils.js
+++ packages/shared/dist/lib/utils.js
@@ -1,19 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.words = exports.getWordArray = exports.alpha = exports.wait = exports.map = void 0;
-//? fait la regle de trois pour calculer 
 function map(x, in_min, in_max, out_min, out_max) {
     return Math.max(Math.min((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min, out_max), out_min);
 }
 exports.map = map;
-//? attend ms de temps
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 exports.wait = wait;
-//? tableau contenant l'alphabet
 exports.alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//? transforme un mot en tableau de mots
 function getWordArray(word) {
     const wordArray = [];
     for (const letter of word) {
@@ -25,7 +21,6 @@ function getWordArray(word) {
     return wordArray;
 }
 exports.getWordArray = getWordArray;
-//? tableau contenant les mots
 exports.words = [
     "CLOUD COMPUTING",
     "BIG DATA",
